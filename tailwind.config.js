4864b2505c63114aa11260a1f1d59d19585cca13
codinci <dkibau@gmail.com>
2023-04-28 22:32:19 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue, js, tx, jsx tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'weather-primary': '#0068A',
+        'weather-secondary':'#004E71'
+      }
+    },
+    fontFamily: {
+      Roboto:['Roboto', 'sans-serif']
+    },
+    container: {
+      padding: '2rem',
+      center: true
+    },
+    screens: {
+      sm: '640px',
+      md: '768px'
+    }
+  },
+  plugins: [],
+}
+
